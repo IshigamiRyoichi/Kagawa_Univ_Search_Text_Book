a3c1614aaaf33b69e8dc57addaf41e6f8696a4fa
IshigamiRyoichi <s19t301@kagawa-u.ac.jp>
2023-08-29 00:44:29 -0700

    Google スプレッドシートの書き込み

--- Search_Text_Book.py
+++ Search_Text_Book.py
@@ -6,6 +6,8 @@ import requests
 import pandas as pd
 import json
 import time
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 open_json = open("./json/my_id_and_pass.json")
 session = json.load(open_json)
@@ -49,7 +51,7 @@ def get_Syllabus_list(chrome_driver):
             syllabus_list.append(href)
     return syllabus_list 
 
-def get_Text_Books(url):
+def get_Text_Book(url):
     df_text_book = pd.read_html(url, match="教科書・参考書等")
     return df_text_book[-1].iloc[1,0]
 
@@ -60,8 +62,35 @@ def get_lesson_name(url):
     lesson_name = soup.select("#ctl00_phContents_sylSummary_txtSbjName")
     return str(lesson_name[0])[50:-7]
 
+def create_lesson_name_and_Text_Book_list(syllabus_list):
+    lesson_name_and_Text_Book_list = []
+    for syllabus in syllabus_list:
+        text_book = get_Text_Book(syllabus)
+        lesson_name = get_lesson_name(syllabus)
+        lesson_name_and_Text_Book_list.append([lesson_name, text_book])
+    return lesson_name_and_Text_Book_list
+
+def insert_data_to_spread_sheet(lesson_name_and_Text_Book_list):
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("./json/search-text-books-4f7b032614d3.json", scope)
+    client = gspread.authorize(creds)
+
+    url = "https://docs.google.com/spreadsheets/d/***"
+    sheet = client.open_by_url(url).sheet1
+
+    row = ["講義名", "教科書・参考書等"]
+    sheet.insert_row(row, 1)
+
+    index = 2
+    for lesson_name_and_Text_Book in lesson_name_and_Text_Book_list:
+        print(lesson_name_and_Text_Book)
+        sheet.insert_row(lesson_name_and_Text_Book, index)
+        index += 1
+
 if __name__ == "__main__":
     chrome_driver = set_up_chrome_driver()
     login_Dream_Canpas(chrome_driver)
     syllabus_list = get_Syllabus_list(chrome_driver)
     chrome_driver.close()
+    lesson_name_and_Text_Book_list = create_lesson_name_and_Text_Book_list(syllabus_list)
+    insert_data_to_spread_sheet(lesson_name_and_Text_Book_list)
